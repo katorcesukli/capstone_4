b6c21f372ded6a996aeff09255292af0d9bd0e5c
Rian <rianestrellado3@gmail.com>
2026-02-17 09:58:07 +0800

    Set up, gradle

---
+++ src/main/java/com/example/capstone_4/Capstone4Application.java
@@ -0,0 +1,15 @@
+package com.example.capstone_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Capstone4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Capstone4Application.class, args);
+
+        //hello test
+    }
+
+}
